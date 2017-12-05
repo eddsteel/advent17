5362290651667a217ef543b90df97086911df9ce
Edd Steel <edward.steel@gmail.com>
2017-12-04 23:17:26 -0800

    Fix build

--- src/test/scala/challenges/Challenge3.scala
+++ src/test/scala/challenges/Challenge3.scala
@@ -3,10 +3,10 @@ import _root_.org.scalatest._
 
 class Challenge3Test extends FlatSpec {
   "challenge 3" should "follow examples" in {
-    assert(Challenge3.blockDistance(1) === Right(0))
-    assert(Challenge3.blockDistance(12) === Right(3))
-    assert(Challenge3.blockDistance(23) === Right(2))
-    assert(Challenge3.blockDistance(1024) === Right(31))
+    assert(Challenge3.blockDistance(1) === Some(0))
+    assert(Challenge3.blockDistance(12) === Some(3))
+    assert(Challenge3.blockDistance(23) === Some(2))
+    assert(Challenge3.blockDistance(1024) === Some(31))
 
     val grid = Challenge3.Grid.from((0, 0), (1, 0), (1, 1), (0, 1), (-1, 1))
     println(grid)
